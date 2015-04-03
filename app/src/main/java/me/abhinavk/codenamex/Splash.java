237f3ff195bcd6672698415751f28e0290ea4767
Abhinav Kumar <abhinavkumar@outlook.com>
2015-04-04 04:20:55 +0530

    Fixed missing button

--- app/src/main/java/me/abhinavk/codenamex/Splash.java
+++ app/src/main/java/me/abhinavk/codenamex/Splash.java
@@ -116,7 +116,6 @@ public class Splash extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
     }
 
