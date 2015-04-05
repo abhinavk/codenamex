ea28e6162983b68291a47bad9905ff7f338c55d0
Prateek <joshi.prateek15@gmail.com>
2015-04-04 18:24:49 -0700

    dashboard ready

---
+++ app/src/main/java/me/abhinavk/codenamex/Category.java
@@ -0,0 +1,15 @@
+package me.abhinavk.codenamex;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Category extends ActionBarActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
