92601aff9fadb703b9b5a2eeb0ad782a13d177de
Abhinav Kumar <abhinavkumar@outlook.com>
2015-04-05 06:58:58 +0530

    Login working

--- app/src/main/java/me/abhinavk/codenamex/MainActivity.java
+++ app/src/main/java/me/abhinavk/codenamex/MainActivity.java
@@ -1,9 +1,40 @@
 package me.abhinavk.codenamex;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,6 +44,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText emailfield = (EditText)findViewById(R.id.emailbox);
+        final EditText pwdfield = (EditText)findViewById(R.id.pwdbox);
+        Button loginbtn = (Button)findViewById(R.id.loginbtn);
+        View.OnClickListener loginbtnlsnr = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new loginAuthentication(MainActivity.this).execute(emailfield.getText().toString(),pwdfield.getText().toString());
+            }
+        };
+        loginbtn.setOnClickListener(loginbtnlsnr);
     }
 
 
@@ -36,6 +78,76 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class loginAuthentication extends AsyncTask<String, Void, JSONObject> {
+
+        private Activity activity;
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://cnxcnx.byethost3.com/login.php");
+            List<NameValuePair> postparams = new ArrayList<NameValuePair>();
+            postparams.add(new BasicNameValuePair("login_email",params[0]));
+            postparams.add(new BasicNameValuePair("login_password",params[1]));
+            InputStream istream = null;
+            JSONObject jsonObject = null;
+            try {
+                httpPost.setEntity(new UrlEncodedFormEntity(postparams));
+            } catch(UnsupportedEncodingException e) {
+
+            }
+            try {
+                HttpResponse response = httpClient.execute(httpPost);
+                HttpEntity entity = response.getEntity();
+                istream = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(istream, "UTF-8"));
+                StringBuilder builder = new StringBuilder();
+                for (String line = null; (line = reader.readLine()) != null;) {
+                    builder.append(line).append("\n");
+                }
+                try {
+                    jsonObject = new JSONObject(builder.toString());
+                } catch (JSONException e) {
+                    Log.d("JSON","No json");
+                }
+            } catch(IOException e) {
+                Log.d("JSON","No io");
+            }
+            return jsonObject;
+        }
+
+        public loginAuthentication(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getApplicationContext(), "Logging in", Toast.LENGTH_LONG).show();
+        }
+        public final static String EXTRA_MSG = "me.abhinavk.codenamex.MESSAGE";
+        @Override
+        protected void onPostExecute(JSONObject data) {
+            super.onPostExecute(data);
+            String email = "unknown";
+            try {
+                if(data.length() > 0) {
+                    email = data.getString("email");
+                }
+            } catch (JSONException e) {
+
+            }
+            if(email == "unknown") {
+                Toast.makeText(getApplicationContext(), "Login failed. Try again.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Logged in as " + email, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(activity, Splash.class);
+                intent.putExtra(EXTRA_MSG, data.toString());
+                startActivity(intent);
+            }
+
+        }
     }
 }
 //
--- app/src/main/java/me/abhinavk/codenamex/Splash.java
+++ app/src/main/java/me/abhinavk/codenamex/Splash.java
@@ -25,7 +25,7 @@ public class Splash extends Activity {
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
      */
     private static final boolean AUTO_HIDE = true;
-    private static final int SPLASH_SHOW_TIME = 5000;
+    private static final int SPLASH_SHOW_TIME = 1000;
 
     /**
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
